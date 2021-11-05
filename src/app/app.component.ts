c423d7545815d6a8afa09c3049704e5fa86dcb32
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-05 13:23:42 -0400

    188. TD: Submitting and Using the Form with (ngSubmit) directive and local ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  //should be trigger whenever form submitted by user
+  // onSubmit(form: HTMLFontElement) {  UPDATE after adding ngForm in template:
+  onSubmit(form: NgForm) {
+    // output whatever user entered:
+    console.log("Submitted!", form);
   }
 }
